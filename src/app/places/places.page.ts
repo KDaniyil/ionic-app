3abd8d74774d5d0884bc3b46f8f9bdf5a8cce179
Dani <daniyil.kud@gmail.com>
2024-02-26 00:18:31 +0100

    add tabs

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { search, card } from 'ionicons/icons';
+import { IonRouterLink } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-places',
   templateUrl: './places.page.html',
   styleUrls: ['./places.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonRouterLink],
 })
-export class PlacesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class PlacesPage {
+  constructor() {
+    addIcons({ search, card });
   }
-
 }
