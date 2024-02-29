09854dae7219483429dfb5dc4f20d865391c204b
Daniyil <daniyil.kudryavtsev@studiovatore.com>
2024-02-29 14:08:41 +0100

    swipeable items

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonItemSliding, IonicModule } from '@ionic/angular';
+import { BookingService } from './booking.service';
+import { Booking } from './booking.modelts';
+import { addIcons } from 'ionicons';
+import { trash } from 'ionicons/icons';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class BookingsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  bookings: Booking[] = [];
+  constructor(private bookingService: BookingService) {
+    addIcons({ trash });
   }
 
+  ngOnInit() {
+    this.bookings = this.bookingService.bookings;
+  }
+
+  onCancel(bookingId: string, slidingElement: IonItemSliding) {}
 }
--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { add } from 'ionicons/icons';
+import { add, create } from 'ionicons/icons';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-offers',
@@ -18,11 +19,16 @@ import { Place } from '../place.model';
 export class OffersPage implements OnInit {
   offers: Place[] = [];
 
-  constructor(private placesService: PlacesService) {
-    addIcons({ add });
+  constructor(private placesService: PlacesService, private router: Router) {
+    addIcons({ add, create });
   }
 
   ngOnInit() {
     this.offers = this.placesService.places;
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'offers', 'edit', offerId]);
   }
 }
